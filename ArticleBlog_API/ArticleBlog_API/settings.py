0a57004790a5aeb0cf0eb8e68de5a3d70967f718
Ehsan Karegar Foroogh <ehsan.kf76@gmail.com>
2023-10-19 16:18:26 +0330

    Pillow installed, User model is complete

--- ArticleBlog_API/ArticleBlog_API/settings.py
+++ ArticleBlog_API/ArticleBlog_API/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -30,6 +31,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     # my apps
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -109,6 +111,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- ArticleBlog_API/account/models.py
+++ ArticleBlog_API/account/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .model_managers import UserManager
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    username = models.CharField(verbose_name="username", max_length=50, unique=True)
+    slug = models.CharField(max_length=50, unique=True, editable=False)
+    email = models.EmailField(verbose_name="email address", max_length=255, unique=True)
+    date_of_birth = models.DateField()
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    created_time = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="images/Users")
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name", "date_of_birth"]
+
+    def __str__(self):
+        return f"{self.username} - {self.email}"
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.username)
+        super(User, self).save(*args, **kwargs)
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
