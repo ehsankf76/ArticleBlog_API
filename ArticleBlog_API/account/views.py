015a7f8e18b554d55f3905b1b831df4042ee4b1a
Ehsan Karegar Foroogh <ehsan.kf76@gmail.com>
2023-10-20 20:58:22 +0330

    Admin file is completed in account app

--- ArticleBlog_API/account/views.py
+++ ArticleBlog_API/account/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from .serializers import UserSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
