fc34d83b8335a20e181f3b041dfe9a8a72b13a2c
Ehsan Karegar Foroogh <ehsan.kf76@gmail.com>
2023-10-23 00:37:47 +0330

    Article model created + defined to the admin all the migrations and dataset was deleted due to a bug

--- ArticleBlog_API/blog/models.py
+++ ArticleBlog_API/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from account.models import User
 
 
 class Category(models.Model):
@@ -13,5 +14,23 @@ class Category(models.Model):
         return self.title
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.username)
+        self.slug = slugify(self.title)
         super(Category, self).save(*args, **kwargs)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=50, unique=True, editable=False)
+    create_time = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to="images/Articles", null=True)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category, related_name="articles")
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Article, self).save(*args, **kwargs)
