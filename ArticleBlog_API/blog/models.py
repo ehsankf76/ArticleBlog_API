a6fe390616d9dbc7abdf8236fe61de3fc385bef7
Ehsan Karegar Foroogh <ehsan.kf76@gmail.com>
2023-10-22 23:54:51 +0330

    Category model created

--- ArticleBlog_API/blog/models.py
+++ ArticleBlog_API/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=50, unique=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.username)
+        super(Category, self).save(*args, **kwargs)
