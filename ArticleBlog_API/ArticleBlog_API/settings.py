5486c44e2173551d593e63ff314bb002a5a91b48
Ehsan Karegar Foroogh <ehsan.kf76@gmail.com>
2023-10-22 23:23:47 +0330

    Authentication with email completed

--- ArticleBlog_API/ArticleBlog_API/settings.py
+++ ArticleBlog_API/ArticleBlog_API/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     # installed
     'rest_framework',
     'drf_spectacular',
+    'rest_framework_simplejwt',
 
     # my apps
     'account',
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
--- ArticleBlog_API/account/urls.py
+++ ArticleBlog_API/account/urls.py
@@ -1,9 +1,14 @@
+from django.urls import path
 from .views import UserViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 
-urlpatterns = []
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
 router = DefaultRouter()
 router.register('users', UserViewSet, basename='users')
 urlpatterns += router.urls
