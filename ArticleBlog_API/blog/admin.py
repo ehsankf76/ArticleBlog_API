4023c46e4bf359e6992f297d3b8ab70ccbcf4243
Ehsan Karegar Foroogh <ehsan.kf76@gmail.com>
2023-10-23 22:29:29 +0330

    Define Comment model to the admin page

--- ArticleBlog_API/blog/admin.py
+++ ArticleBlog_API/blog/admin.py
@@ -9,5 +9,10 @@ class ArticleAdmin(admin.ModelAdmin):
     list_filter = ('create_time', 'last_update')
     search_fields = ('title', 'author', 'category')
 
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_filter = ('article')
+
 admin.site.register(models.Category)
 admin.site.register(models.Article, ArticleAdmin)
+admin.site.register(models.Comment)
